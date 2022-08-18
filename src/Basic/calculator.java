641b9a9a3df3c663f636c5318c65afc700f508d4
sohamdagade24 <sohamdagade8198@gmail.com>
2022-08-18 16:05:53 +0530

    files

---
+++ src/Basic/calculator.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class calculator {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Addition");
+        math.add(22, 67);
+	    math.add(10,15,25);
+		System.out.println("Multiplication");
+		math.multiply(10, 5);
+		
+	}
+}
---
+++ src/Basic/math.java
@@ -0,0 +1,20 @@
+
+
+package Basic;
+
+public class math
+{
+	public static void add(int a, int b)
+	{
+		System.out.println("Sum is: "+ a+b);
+	}
+	public static void add(int a,int b,int c)
+	{
+		System.out.println("Sum is: "+ a+b+c);
+	}
+	public static void multiply(int a, int b)
+	{
+		System.out.println("Product is: "+ a*b);
+	}
+
+}
